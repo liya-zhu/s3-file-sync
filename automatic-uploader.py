47d78b513be1f5af74fc0777ae9d6d6cd7978d60
liya-zhu <julia.liya.zhu@gmail.com>
2023-01-13 16:37:04 -0500

    made it auto-detect new files added and sync

--- automatic-uploader.py
+++ automatic-uploader.py
@@ -1,28 +1,76 @@
 import boto3, pyinotify
 import os #access directories on laptop
+from pathlib import Path
 
 client = boto3.client('s3')
+path = '/home/jim/Desktop/tobeuploaded/' #path of the directory to be synced
 
-for file in os.listdir(): #all files in same working dir
+#uploading to s3
+def sync_whole_dir():
+    for file in os.listdir(path): 
+        upload_file_bucket = 'filesyncc'
+        upload_file_key = get_sorted_path(file)
+        client.upload_file(path+file,upload_file_bucket,upload_file_key)
 
+def sync_file(file):
     upload_file_bucket = 'filesyncc'
+    upload_file_key = get_sorted_path(file)
+    client.upload_file(path+file,upload_file_bucket,upload_file_key)
 
+def get_sorted_path(file):
     if '.py' in file:
-        upload_file_key = 'python/' + str(file)
-        client.upload_file(file,upload_file_bucket,upload_file_key)
+            upload_file_key = 'python/' + str(file)
 
     elif '.txt' in file:
         upload_file_key = 'text/' + str(file)
-        client.upload_file(file,upload_file_bucket,upload_file_key)
 
     elif '.pdf' in file:
         upload_file_key = 'pdf/' + str(file)
-        client.upload_file(file,upload_file_bucket,upload_file_key)
 
     elif '.doc' in file or '.docx' in file:
         upload_file_key = 'word_doc/' + str(file)
-        client.upload_file(file,upload_file_bucket,upload_file_key)
     
     else:
         upload_file_key = 'other/' + str(file)
-        client.upload_file(file,upload_file_bucket,upload_file_key)
+    
+    return upload_file_key
+
+
+sync_whole_dir()
+print("Syncing the whole directory", path, "to s3.")
+
+
+# watch the folder for changes using pyinotify
+# The watch manager stores the watches and provides operations on watches
+wm = pyinotify.WatchManager()
+
+mask = pyinotify.IN_DELETE | pyinotify.IN_CREATE  # watched events
+
+class EventHandler(pyinotify.ProcessEvent): #The EventHandler class inherits from a processing base class called ProcessEvent; it handles notifications and takes actions through specific processing methods. For an EVENT_TYPE, a process_EVENT_TYPE function will execute. 
+    def process_IN_CREATE(self, event):
+        print("Creating", event.pathname, "and syncing to s3")
+        sync_file(Path(str(event.pathname)).name)
+
+    def process_IN_DELETE(self, event):
+        print("Removing", event.pathname)
+
+handler = EventHandler()
+notifier = pyinotify.Notifier(wm, handler)
+wdd = wm.add_watch(path, mask, rec=True)
+
+notifier.loop()
+
+
+
+
+
+"""
+next steps:
+make it auto-detect new files added
+should it delete deleted local files?
+
+sync edited files
+
+make it recognize folders and loop through them
+
+"""
